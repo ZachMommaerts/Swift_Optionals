ce5d868329488807905a0658091ab56dbd29a7d9
ZachMommaerts <zmommaerts@gmail.com>
2023-03-05 11:20:51 -0600

    Initial Commit

---
+++ Swift_Optionals/Swift_OptionalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_OptionalsApp.swift
+//  Swift_Optionals
+//
+//  Created by Zach Mommaerts on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_OptionalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
